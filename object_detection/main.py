fixed buffer overflow, will now work on converting radians to x coordinates

object_detection/main.py
@@ -28,7 +28,7 @@ from utils import label_map_util
 from utils import visualization_utils as vis_util
 
 # Rplidar
-from rplidar import RPLidar
+from lam_helper import *
 
 # # Model preparation 
 
@@ -116,33 +116,17 @@ IMAGE_SIZE = (12, 8)
 
 
 PORT_NAME = 'COM15'
+lidar = ''
+while lidar == '':
+    try:
+        lidar = gen()
+    except:
+        exit()
 
-class wrapper(object):
-
-    def __init__(self, generator):
-      self.__gen = generator()
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        self.current = next(self.__gen)
-        return self.current
-
-    def __call__(self):
-        return self
-
-@wrapper
-def gen():
-    lidar = RPLidar(PORT_NAME)
-    for i in lidar.iter_measurments():
-        yield i
-
-lidar = gen()
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
-    while True:
-      lidar_read = list(next(lidar))
+    for a in AsynchronousGenerator(function=gen(), maxsize=0):
+      lidar_read = a
       ret, image_np = cap.read()
       # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
       image_np_expanded = np.expand_dims(image_np, axis=0)